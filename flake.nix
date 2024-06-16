f4d40f5d25843ab5ab28c1913c5c922963f0081b
kilimnik <mail@kilimnik.de>
2024-06-16 21:00:52 +0200

    Update nix

--- flake.nix
+++ flake.nix
@@ -1,50 +1,55 @@
 {
+  description = "Application packaged using poetry2nix";
+
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable-small";
     poetry2nix = {
       url = "github:nix-community/poetry2nix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = { self, nixpkgs, poetry2nix }:
-    let
-      supportedSystems = [ "x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin" ];
-      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
-      pkgs = forAllSystems (system: (import nixpkgs {
-        inherit system;
-        overlays = [
-          poetry2nix.overlay
-        ];
-      }));
+  outputs = { self, nixpkgs, flake-utils, poetry2nix }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        # see https://github.com/nix-community/poetry2nix/tree/master#api for more functions and examples.
+        pkgs = nixpkgs.legacyPackages.${system}.extend poetry2nix.overlays.default;
 
-      overrides = pkgs: pkgs.poetry2nix.overrides.withDefaults (pyself: pysuper: {
-        pilmoji = pysuper.pilmoji.overridePythonAttrs (oldAttrs: {
-          format = "setuptools";
-          prePatch = ''sed -z -i "s/with open('requirements.txt') as fp:\n    requirements = fp.readlines()/requirements=\[\"Pillow\", \"emoji\"\]/" setup.py'';
+        overrides = pkgs.poetry2nix.overrides.withDefaults (pyself: pysuper: {
+          pilmoji = pysuper.pilmoji.overridePythonAttrs (oldAttrs: {
+            format = "setuptools";
+            prePatch = ''sed -z -i "s/with open('requirements.txt') as fp:\n    requirements = fp.readlines()/requirements=\[\"Pillow\", \"emoji\"\]/" setup.py'';
+          });
         });
-      });
-    in
-    {
-      packages = forAllSystems
-        (system: {
-          default = pkgs.${system}.poetry2nix.mkPoetryApplication {
-            projectDir = ./.;
-            overrides = overrides pkgs.${system};
+
+        inherit (poetry2nix.lib.mkPoetry2Nix { inherit pkgs; }) mkPoetryApplication;
+      in
+      {
+        packages = {
+          abschieben = mkPoetryApplication {
+            inherit overrides;
+            projectDir = self;
           };
-        });
-      devShells = forAllSystems (system: {
-        default = pkgs.${system}.mkShell {
-          buildInputs = with pkgs.${system}; [
-            poetry
-            zlib
-            (pkgs.${system}.poetry2nix.mkPoetryEnv {
-              projectDir = ./.;
-              overrides = overrides pkgs.${system};
-              #   editablePackageSources = { };
-            })
-          ];
+          default = self.packages.${system}.abschieben;
+        };
+
+        # Shell for app dependencies.
+        #
+        #     nix develop
+        #
+        # Use this shell for developing your app.
+        devShells.default = pkgs.mkShell {
+          inputsFrom = [ self.packages.${system}.abschieben ];
+        };
+
+        # Shell for poetry.
+        #
+        #     nix develop .#poetry
+        #
+        # Use this shell for changes to pyproject.toml and poetry.lock.
+        devShells.poetry = pkgs.mkShell {
+          packages = [ pkgs.poetry ];
         };
       });
-    };
 }
